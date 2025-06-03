368fdb227a773a2e3bdd660e753012a91cd0ab2c
Rafidsyakir <rafidsyakir30@gmail.com>
2025-06-03 20:41:15 +0700

    Delete Kelas_10 directory

--- Kelas_10/kelas/2024-11-5/pages/contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $sql = "SELECT * FROM contact";
-    echo $sql;
-?>
-<div class="contact">
-    <h2>Contact</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-       Quis totam est saepe modi rerum sint ab voluptate dignissimos non voluptatem sed, deserunt esse officia deleniti dolore similique mollitia ipsum sequi?</p>
-</div>
--- Kelas_10/kelas/2024-11-5/pages/sejarah.php
+++
@@ -1,5 +0,0 @@
-<div class="sejarah">
-    <h2>Sejarah</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-       Necessitatibus harum voluptatum voluptates itaque fugiat adipisci debitis eveniet vel. Deserunt nobis libero aperiam quo praesentium incidunt dolorum fugiat unde sapiente nihil.</p>
-</div>
